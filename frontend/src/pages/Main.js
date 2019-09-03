16b2f90b4158f81f9ba36cbc079f2444cd22291d
fagner tonon <fagnertonon@hotmail.com>
2019-09-03 20:47:33 -0300

    add css page main

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,61 @@
 import React from "react";
+import './Main.css'
+import logo from '../assets/tindev.svg'
+import like from '../assets/like.svg'
+import dislike from '../assets/dislike.svg'
 
 export default function Main(match) {
     return (
-        <h1>MAIN</h1>
+        <div className="main-container">
+            <img src={logo} alt="Tindev" />
+            <ul>
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/16415868?v=4" alt=""></img>
+                    <footer>
+                        <strong>Fagner</strong>
+                        <p>teste    </p>
+                    </footer>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={like} alt="Like" className="like" />
+                        </button>
+                        <button type="button">
+                            <img src={dislike} alt="Dislike" className="dislike" />
+                        </button>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/16415868?v=4" alt=""></img>
+                    <footer>
+                        <strong>Fagner</strong>
+                        <p>teste    </p>
+                    </footer>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={like} alt="Like" className="like" />
+                        </button>
+                        <button type="button">
+                            <img src={dislike} alt="Dislike" className="dislike" />
+                        </button>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/16415868?v=4" alt=""></img>
+                    <footer>
+                        <strong>Fagner</strong>
+                        <p>teste    </p>
+                    </footer>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={like} alt="Like" className="like" />
+                        </button>
+                        <button type="button">
+                            <img src={dislike} alt="Dislike" className="dislike" />
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
     )
 }
